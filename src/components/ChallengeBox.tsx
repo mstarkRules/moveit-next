819654c5202b924d48a269a971fb9cae6c27b90b
Marcos Amorim <markstm10@gmail.com>
2021-03-30 23:37:54 -0300

    🛠 Criado funções no contexto para iniciar novo desafio e colocado calculo de xp

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -4,27 +4,25 @@ import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox(){
 
-    const contextData = useContext(ChallengesContext);
+    const {activeChallenge, resetChallenge} = useContext(ChallengesContext);
 
-    const hasActiveChallenge = true;
-
-    console.log(contextData);
     
     return(
         <div className={styles.challengeBoxContainer}>
-            {hasActiveChallenge ? (
+            {activeChallenge ? (
                 <div className={styles.challengeActive}>
-                    <header>Ganhe 400 xp</header>
+                    <header>Ganhe {activeChallenge.amount} xp</header>
 
                     <main>
-                        <img src="icons/body.svg"/>
+                        <img src={`icons/${activeChallenge.type}.svg`}/>
                         <strong>Novo desafio</strong>
-                        <p>Levante e faça uma caminhada de 3 minutos</p>
+                        <p>{activeChallenge.description}</p>
                     </main>
                         <footer>
                             <button
                                 type="button"
                                 className={styles.challengeFailedButton}
+                                onClick={resetChallenge}
                             >
                                 Falhei
                             </button>
